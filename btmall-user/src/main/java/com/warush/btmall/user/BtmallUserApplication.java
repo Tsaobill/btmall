413465686e0e698f219eb8afb8369ba830b8dbac
bill <bill>
2019-11-03 19:04:24 +0800

    新建用户服务

---
+++ btmall-user/src/main/java/com/warush/btmall/user/BtmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.warush.btmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (BtmallUserApplication.class, args);
+    }
+
+}
